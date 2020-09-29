67ccc6c6ffe677ea3ccc30987a0f380d6d1b2663
HeberPV <183432@ids.upchiapas.edu.mx>
2020-09-28 22:54:06 -0500

    HIPERMEGAKAKA

--- template1/Automata.js
+++ template1/Automata.js
@@ -4,6 +4,8 @@ function Automata() {
     let words = variable.split(" ");
     let end = false;
     let contador = 0;
+    let estadoAcual = 0;
+    let estadosAceptacion = ['12','20','23','31','46','59','62','67','68'];
     
     while(!end){
     if(contador > words.length - 1){
@@ -15,65 +17,216 @@ function Automata() {
             letter = words[contador].charAt(i);
             console.log(letter)
             console.log("i: "+i+"    contador:"+contador)
-            switch (letter) {
-                case 'c':
-                    break;
+            switch(estadoAcual){
+                case 0:
+                break;
+        
+                case 1:
+                break;
+        
+                case 2:
+                break;
+        
+                case 3:
+                break;
+        
+                case 4:
+                break;
+        
+                case 5:
+                break;
+        
+                case 6:
+                break;
+        
+                case 7:
+                break;
+        
+                case 8:
+                break;
+        
+                case 9:
+                break;
+        
+                case 10:
+                break;
+        
+                case 11:
+                break;
+        
+                case 12:
+                break;
+        
+                case 13:
+                break;
+        
+                case 14:
+                break;
+        
+                case 15:
+                break;
+        
+                case 16:
+                break;
+        
+                case 17:
+                break;
+        
+                case 18:
+                break;
+        
+                case 19:
+                break;
+        
+                case 20:
+                break;
+        
+                case 21:
+                break;
+        
+                case 22:
+                break;
+        
+                case 23:
+                break;
+        
+                case 24:
+                break;
+        
+                case 25:
+                break;
+        
+                case 26:
+                break;
+        
+                case 27:
+                break;
+        
+                case 28:
+                break;
+        
+                case 29:
+                break;
+        
+                case 30:
+                break;
+        
+                case 31:
+                break;
+        
+                case 32:
+                break;
+        
+                case 33:
+                break;
+        
+                case 34:
+                break;
+        
+                case 35:
+                break;
+        
+                case 36:
+                break;
+        
+                case 37:
+                break;
+        
+                case 38:
+                break;
+        
+                case 39:
+                break;
+        
+                case 40:
+                break;
+        
+                case 41:
+                break;
+        
+                case 42:
+                break;
+        
+                case 43:
+                break;
+        
+                case 44:
+                break;
+        
+                case 45:
+                break;
+        
+                case 46:
+                break;
+        
+                case 47:
+                break;
+        
+                case 48:
+                break;
+        
+                case 49:
+                break;
+        
+                case 50:
+                break;
+        
+                case 51:
+                break;
+        
+                case 52:
+                break;
+        
+                case 53:
+                break;
+        
+                case 54:
+                break;
+        
+                case 55:
+                break;
+        
+                case 56:
+                break;
+        
+                case 57:
+                break;
+        
+                case 58:
+                break;
+        
+                case 59:
+                break;
+        
+                case 60:
+                break;
+        
+                case 61:
+                break;
+        
+                case 62:
+                break;
+        
+                case 63:
+                break;
+        
+                case 64:
+                break;
+        
+                case 65:
+                break;
+        
+                case 66:
+                break;
+        
+                case 67:
+                break;
+        
+                case 68:
+                break;
 
-                case 'l':
-                    break;
-
-                case 'e':
-                    break;
-
-                case 'a':
-                    break;
-
-                case 'r':
-                    break;
-
-                case 'o':
-                    break;
-
-                case 'o':
-                    break;
-
-                case 'n':
-                    break;
-
-                case 'd':
-                    break;
-
-                case 't':
-                    break;
-
-                case 'i':
-                    break;
-
-                case 'h':
-                    break;
-
-                case 'g':
-                    break;
-
-                case 'm':
-                    break;
-
-                case 's':
-                    break;
-
-                case 'u':
-                    break;
-
-                case 'p':
-                    break;
-
-                case 'z':
-                    break;
-
-                case 'b':
-                    break;
                 default:
-                    break;
+                break;
                 }
                 i++;
             }
